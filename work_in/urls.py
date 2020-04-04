56b53f5b2e20287219580a360d64f7e31ef88bb6
t <38640424+poohcid@users.noreply.github.com>
2020-04-04 15:26:16 +0700

    work in

--- work_in/urls.py
+++ work_in/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.here_or_home, name="here_or_home"),
-    path('table/', views.table, name="table"),
+    path('', views.table, name="here_or_home"),
+    path('table/', views.here_or_home , name="table"),
     path('create_table/', views.create_table, name="create_table"),
-    
-
 ]
