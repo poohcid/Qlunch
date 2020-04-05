e70f94148324e01ce8178040203d1a39db23801d
t <38640424+poohcid@users.noreply.github.com>
2020-04-05 12:28:21 +0700

    create order ได้แล้ว

--- work_in/admin.py
+++ work_in/admin.py
@@ -6,13 +6,26 @@ from django.contrib.auth.models import Permission
 
 # Register your models here.
 
+def select_true(modeladmin, request, queryset):
+    queryset.update(status=True)
+
+def select_false(modeladmin, request, queryset):
+    queryset.update(status=False)
+
+class TableAdmin(admin.ModelAdmin):
+    list_display = ["id", "status"]
+    actions = [select_true, select_false]
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "order_type"]
+
 admin.site.register(Customer)
 
 admin.site.register(Food)
 
-admin.site.register(Table)
+admin.site.register(Table, TableAdmin)
 
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 
 admin.site.register(Order_food)
 
--- work_in/views.py
+++ work_in/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render, redirect
-from appModel.models import Table, Order
-from work_in.forms import TableForm, OrderForm
+
+from datetime import datetime
+
+from django.shortcuts import redirect, render
+
+from appModel.models import Order, Table, Order_in
+from .forms import OrderForm, TableForm
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 
@@ -10,25 +16,40 @@ def create_table(request):
         form = TableForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('table')
+    
+    return redirect('here_or_home')
+    
 
 
 def create_order(request):
     context = {}
     if request.method == "POST":
-        form = OrderForm(request.POST)
+        order1 = Order.objects.create(
+            date_book = datetime.now(),
+            total_price = 0.0,
+            employee = request.user,
+            order_type = "order_in",
+        )
+        form = OrderForm(request.POST, instance=order1)
         if form.is_valid():
-            form.save()
-            return redirect('table')
+            order1 = form.save()
+            order_in1 = Order_in.objects.create(
+                order = order1
+            )
+            for i in request.POST.get("count_table"):
+                table1 = Table.objects.get(pk=int(i))
+                table1.status = True
+                table1.save()
+                order_in1.table.add(table1)
+        else:
+            order1.delete()
 
+    return redirect('here_or_home')
 
-def here_or_home(request):
-    return render(request, template_name='work_in/here_or_home.html')
-
-
+#index work in
 def table(request):
     context = {}
-    table = Table.objects.all()
+    table = Table.objects.all().order_by('id')
     form_order = OrderForm()
     context['table'] = table
     context['form_order'] = form_order
@@ -38,7 +59,7 @@ def table(request):
 
 def at_store(request):
     context = {}
-    table = Table.objects.all()
+    table = Table.objects.all().order_by('id')
     form_order = OrderForm()
     context['table'] = table
     context['form_order'] = form_order
