94b8b245e442665d0fb7ce08c071c392f53d6bb5
t <38640424+poohcid@users.noreply.github.com>
2020-04-16 20:30:28 +0700

    start buffet

--- Qlunch/settings.py
+++ Qlunch/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'appModel',
     'work_in',
+    'buffet'
 ]
 
 MIDDLEWARE = [
--- Qlunch/urls.py
+++ Qlunch/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('appModel.urls')),
     path('admin/', admin.site.urls),
-    path('work_in/', include('work_in.urls'))
+    path('work_in/', include('work_in.urls')),
+    path('buffet/', include('buffet.urls'))
 ]
